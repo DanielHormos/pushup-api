92e78399eb08b9f187c02bc75fba2ca6a15d3a84
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-02 12:51:34 +0100

    removed mock database and created db type for the input inside createSession feature

--- src/features/pushup-session/index.ts
+++ src/features/pushup-session/index.ts
@@ -1,26 +1,18 @@
 import { Router } from "express";
+import { Session } from "./types";
 
-const db = [
-  {
-    session_uuid: "abc123",
-    repetitions: 50,
-    date: "2024-11-01",
-    notes: "Improved my form",
-  },
-  {
-    session_uuid: "def456",
-    repetitions: 30,
-    date: "2024-11-02",
-    notes: "Felt a bit tired",
-  },
-];
+type Db = {
+  getAll: () => Promise<Session[]>;
+  add: (session: Session) => Promise<void>;
+};
 
-export function createPushupSessionFeature() {
+export function createSessionFeature(db: Db) {
   return {
     getRouter() {
       const router = Router();
       router.get("/", (req, res) => {
-        res.json(db);
+        //async?
+        res.json(db.getAll());
       });
 
       return router;
