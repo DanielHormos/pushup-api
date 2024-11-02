710e7d6bdaa03e665d133cd4b615a4365dc33fad
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-02 12:58:58 +0100

    added startapp function file that creates app on port 3000

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
- 
+import { createApp } from "./app/app";
+
+export function startApp() {
+  createApp().listen(3000, () => {
+    console.log("App is running on http://localhost:3000");
+  });
+}
+
+startApp();
