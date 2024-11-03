d16089f5ca525e4c03cdb2e61613b19e196cbbe7
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-03 13:52:30 +0100

    separated creating db for each feature to another folder

---
+++ src/app/create-db/createSessionDb.ts
@@ -0,0 +1,37 @@
+import { Session } from "../../features/session/types";
+
+function createSessionDb() {
+  const sessions: Session[] = [];
+
+  return {
+    getAll: async () => sessions,
+    getById: async (uuid: string) => {
+      return sessions.find((session) => session.sessionUuid === uuid) || null;
+    },
+    add: async (session: Session) => {
+      sessions.push(session);
+    },
+    delete: async (uuid: string): Promise<void> => {
+      const index = sessions.findIndex(
+        (session) => session.sessionUuid === uuid
+      );
+      if (index === -1) {
+        throw new Error("Session not found");
+      }
+      sessions.splice(index, 1);
+    },
+    patch: async (
+      uuid: string,
+      updatedData: Partial<Session>
+    ): Promise<void> => {
+      const index = sessions.findIndex(
+        (session) => session.sessionUuid === uuid
+      );
+      if (index === -1) {
+        throw new Error("Session not found");
+      }
+
+      sessions[index] = { ...sessions[index], ...updatedData };
+    },
+  };
+}
